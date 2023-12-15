e6abe389bb54a7692f06e51afea1796e9207bb2e
“Sebastian <gabrielsebastian.romero@gmail.com>
2023-12-15 11:05:24 -0500

    added display message

--- main.py
+++ main.py
@@ -32,11 +32,10 @@ def main():
             if event.type == pygame.QUIT:
                 exit()
 
-            #
+            # Player's turn
             elif event.type == pygame.MOUSEBUTTONDOWN and not game_over and my_turn:
                 # mark the appropriate tile
                 # get the position of the mouse
-
                 mouse_x, mouse_y = pygame.mouse.get_pos()
 
                 for col in range(B_COLS):
@@ -55,14 +54,9 @@ def main():
                 pygame.display.flip()
                 pygame.display.update()
                 status = game_mngmt.check_gamestatus(game_mngmt.board.board_squares)
-                if status == TERMINAL_STATE.MAX_WON or \
-                        status == TERMINAL_STATE.MIN_WON or \
-                        status == TERMINAL_STATE.TIE_GAME:
-                    pygame.display.update()
-                    time.sleep(4)
-                    sys.exit()
+                check_terminal(status)
             
-            # #AI turn
+            # #AI's turn
             elif not game_over and not my_turn:
                 move= minmax_decision_pruning(game_mngmt.board.board_squares)
                 x,y=move
@@ -73,13 +67,27 @@ def main():
                 pygame.display.flip()
                 pygame.display.update()
                 status = game_mngmt.check_gamestatus(game_mngmt.board.board_squares)
-                if status == TERMINAL_STATE.MAX_WON or \
-                        status == TERMINAL_STATE.MIN_WON or \
-                        status == TERMINAL_STATE.TIE_GAME:
-                    pygame.display.update()
-                    time.sleep(4)
-                    sys.exit()
+                check_terminal(status)
 
+def check_terminal(status):
+    terminal_statuses = [TERMINAL_STATE.MAX_WON, TERMINAL_STATE.MIN_WON, TERMINAL_STATE.TIE_GAME]
+
+    if status in terminal_statuses:
+        font = pygame.font.Font(None, 50)
+        if status == TERMINAL_STATE.MAX_WON:
+            message = "Player One Wins!"
+        elif status == TERMINAL_STATE.MIN_WON:
+            message = "Player Two Wins!"
+        else:
+            message = "It's a Tie!"
+
+        text = font.render(message, True, (0,0,255))
+        text_rect = text.get_rect(center=(window.get_width() // 2, window.get_height() // 4))
+
+        window.blit(text, text_rect)
+        pygame.display.flip()
+        time.sleep(4)
+        sys.exit()
 
 
 
